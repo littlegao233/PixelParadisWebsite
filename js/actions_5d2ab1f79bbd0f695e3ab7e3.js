7d81994b3ec8438d12fd7d0d65de7cd373d63f11
gxh <46601807+littlegao233@users.noreply.github.com>
2020-03-08 13:29:38 +0800

    old

---
+++ js/actions_5d2ab1f79bbd0f695e3ab7e3.js
@@ -0,0 +1,11 @@
+Mugeda.script.push(function (mugeda) {
+(function(mugeda){
+//if (confirm("欢迎访问像素彼岸官网"))
+//    {
+    alert("欢迎访问像素彼岸官网！")
+//    test.innerHTML="欢迎访问像素彼岸官网"
+//    }
+//window.open("minecraft:?addExternalServer=gxh临时服务器|mcgxh.napy.xyz:19132")
+}).call(mugeda, mugeda);
+
+});
---
+++ js/manifest.js
@@ -0,0 +1,15 @@
+(function () {  
+
+_mrmcp = (typeof _mrmcp == 'undefined') ? {} : _mrmcp;
+_mrmcp['campaign_id'] = 'none';
+_mrmcp['owner_id'] = '59ce147192b5792aee6d68ad';
+_mrmcp['creative_id'] = '5d2ab1f79bbd0f695e3ab7e3';
+_mrmcp['ga_url'] = _mrmcp['ga_url'] || (('https:' == document.location.protocol ? 'https://' : 'http://') + 'card.mugeda.com/weixin/card/ga.js');
+_mrmcp['width'] = _mrmcp['width'] || 320;
+_mrmcp['height'] = _mrmcp['height'] || 520;
+_mrmcp['type'] = 'smart';
+_mrmcp['title'] = 'MCBE像素彼岸服务器官网';
+_mrmcp['build_number'] = parseInt('316');
+_mrmcp['publish_time'] = 'Sun Mar 08 2020 17:37:35 GMT+0800 (CST)';
+_mrmcp['track_bot'] = 'http://cdn.mugeda.com/media/pages/track/track_20131030.html';
+})();
